36e051b77c867fca939e83ced26dfe627b8e79c2
"Joseph Bejarano" <"joseph.bejarano@correounivalle.edu.co">
2017-12-10 15:56:11 -0500

    Merge origin/master
    Conflicts:
    	resources/views/activo/eliminaciones.blade.php
    	resources/views/activo/prestamos.blade.php
    	resources/views/templates/templateConsultas.blade.php
    	resources/views/templates/templateMain.blade.php

--- src/Paquete/Villa.java
+++ src/Paquete/Villa.java
@@ -1,19 +1,21 @@
 package Paquete;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Villa {
 
     public ArrayList enemi;
     public ArrayList pais;
-    public String[] villa;
+    public String[] villas;
+    String[] X;
     int nP = 0;
     int nE = 0;
 
     public Villa(ArrayList enemi, ArrayList pais) {
         this.enemi = enemi;
         this.pais = pais;
-        this.villa = new String[pais.size()];
+        this.villas = new String[pais.size()];
     }
 
     public ArrayList getEnemi() {
@@ -31,42 +33,60 @@ public class Villa {
     public void setPais(ArrayList pais) {
         this.pais = pais;
     }
-    
-    public Villa newVilla(){
-       Villa x = new Villa(enemi, pais);
-        return x;
+
+    public String newVilla(ArrayList l1) {
+        String X = null;
+        for (int i = 0; i < l1.size(); i++) {
+            villas[i] = l1.get(i).toString();
+            
+            X = "villa" + i + " = " + villas[i] ;
+
+            return X;
+        }
+
+        return "";
     }
 
     public String[] insertarVilla() {
         for (int i = 0; i < pais.size(); i++) {
-           
 
             if (pais.get(i) != null) {
-                
+
                 nP = i;
-                
+
             }
             if (enemi.get(i) != null) {
-             
+
                 nE = i;
-                
+
             }
-            
-            if (nE==nP){
+        }
+
+        if (nE == nP) {
+
+            System.out.println(this.newVilla(pais));
+
+            for (int i = 1; i < pais.size(); i++) {
+
+                X = new String[pais.size()];
                 for (int j = 0; j < 10; j++) {
-                    if(villa[j]==enemi.get(j)){
-                        this.newVilla();
-                            
-                    }else{
-                        villa[j]=enemi.get(i).toString();
-                    }
+                String temp = enemi.get(j).toString();
+                String Y =temp.replace("[", "");
+                String Z = Y.replace("]", "");
+                X = Z.split("",1);
+                
+                     if(X==pais.get(i)){
+                   
+                     }
                 }
-                villa[i]=pais.get(i).toString();
+               
+                System.out.println(Arrays.toString(X));
+//                    if (pais.get(i)=)
             }
 
         }
- 
-        return villa;
+
+        return villas;
     }
 
 }
